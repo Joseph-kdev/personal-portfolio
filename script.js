90d4c7f735086577ea395b75fca96a8aaf0f2e61
Joseph Kinuthia <cjkinuthia5@gmail.com>
2024-03-15 16:54:34 +0300

    complete revamp

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuIcon = document.getElementById('menu-icon');
+const nav = document.querySelector('.nav');
+const links = document.querySelectorAll('.nav a');
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menuIcon.checked = false; // Uncheck the checkbox
+  });
+});
