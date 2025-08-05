2b650491039577ca2dfd4de36896570c53fb8a04
joseph-kdev <cjkinuthia5@gmail.com>
2025-08-05 16:01:39 +0300

    reworked intro, about and what I do animations

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ links.forEach(link => {
   });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const image = document.querySelector(".profile-img");
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target)
+      }
+    })
+  }, {threshold: 0.9})
+  observer.observe(image)
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  const meIntro = document.querySelector(".me-intro");
+  const whatItem = document.querySelectorAll(".what-I-do-item")
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target)
+      } else {
+        entry.target.classList.remove("visible")
+      }
+    })
+  }, {threshold: 0.001})
+  observer.observe(meIntro)
+  whatItem.forEach(item => observer.observe(item))
+})
